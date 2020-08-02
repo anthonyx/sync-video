1c96bc89f9a84b1213f6b932b350b374d4da1afe
Anthony Xie <anthonyx.mozilla@gmail.com>
2020-08-01 17:59:31 -0700

    Add socket.io and youtube player

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/static'));
 
-app.listen(3000);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log("jay chou server is listening")
+});
